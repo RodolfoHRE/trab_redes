3fe8d3b9702bdea6bd459ed5ef1cd2ab6ba0ed20
RodolfoHRE <64434994+RodolfoHRE@users.noreply.github.com>
2023-11-15 10:38:35 -0300

    Plot de gráficoalterado, falta somente passar as colunas para executar a função de criação dos respectivos gráficos

--- module/data_display.py
+++ module/data_display.py
@@ -1,1 +1,24 @@
 #Display dos gráficos
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+df = pd.read_csv('teste.csv')
+
+def cria_grafico(str1, str2):
+
+    print(df.head())
+
+    plt.scatter(df[str1], df[str2])
+
+    plt.xlabel(str1)
+    plt.ylabel(str2)
+
+    plt.title("Gráfico"+ str2 + "X tempo")
+
+    plt.show()
+
+    print(df.head())
+
+cria_grafico('coluna_x','coluna_y')
